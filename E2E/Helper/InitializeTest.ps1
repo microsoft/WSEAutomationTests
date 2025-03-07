70168b39270ec66e78ac20f57b06f2c371bea96d
Tapas Sarkar <aastitva@gmail.com>
2025-03-07 10:55:46 +0530

    missed_code_added

--- E2E/Helper/InitializeTest.ps1
+++ E2E/Helper/InitializeTest.ps1
@@ -22,7 +22,8 @@ function InitializeTest($TstsetNme, $targetMepCameraVer, $targetMepAudioVer, $ta
 	$Global:escapedpythonLibPath = (Resolve-Path $pythonLibFolder).Path -replace '\\', '\\'
     $Global:SequenceNumber = 0
     $Global:Results = '' | SELECT ScenarioName,FramesAbove33ms,AvgProcessingTimePerFrame,MaxProcessingTimePerFrame,MinProcessingTimePerFrame,PCInItTime,CameraAppInItTime,VoiceRecorderInItTime,fps,PCInItTimeForAudio,FramesAbove33msForAudioBlur,PeakWorkingSetSize,AvgWorkingSetSize,Status,ReasonForNotPass
-
+    $Global:validatedCameraFriendlyName = ""
+    
     # once if the WseEnabingStatus validation fails, stop and exit the test
     if ((WseEnablingStatus $targetMepCameraVer $targetMepAudioVer $targetPerceptionCoreVer) -eq $false)
     {
