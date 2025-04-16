03a5c68e3479f2214d13de23bcb2724dd20bcd08
FrankWu <frankwbd@gmail.com>
2025-04-16 15:55:35 +0800

    Initial version of CameraReliabilityTest
    What: Perform a check on continuously switching between photo mode and video mode in the Camera app to ensure that the MEP function remains active and that camera streaming is still available.
    
    Why: To assist silicon and OEM partners in ensuring the reliability of their camera drivers and to maintain the quality of MEP.
    
    How: To run the script, use the command '.\CameraReliabilityTest.ps1 -iteration <integer>'. For example, '.\CameraReliabilityTest.ps1 -iteration 10' will switch between photo and video modes 10 times. If the '-iteration' parameter is not specified by the user, its default value is 10.
    
    Tested how: Executed CameraReliabilityTest.ps1 on Qualcomm/Intel/AMD platforms.

---
+++ E2E/CameraReliabilityTest.ps1
@@ -0,0 +1,133 @@
+param (
+   [string] $token = $null,
+   [string] $SPId = $null,
+   [string] $targetMepCameraVer = $null,
+   [string] $targetMepAudioVer = $null,
+   [string] $targetPerceptionCoreVer = $null,
+   [int]    $iteration = 0
+)
+
+# definition of the scenario name & scenario ID
+Set-Variable -Name "WSE_ALL_CAMERA_EFFECTS_SCENARIO_V1"     -Option ReadOnly -Value "AF+EC+BBP"
+Set-Variable -Name "WSE_ALL_CAMERA_EFFECTS_SCENARIO_ID_V1"  -Option ReadOnly -Value 81968
+Set-Variable -Name "WSE_ALL_CAMERA_EFFECTS_SCENARIO_V2"     -Option ReadOnly -Value "AF+PL+ECE+BBP+CFA"
+Set-Variable -Name "WSE_ALL_CAMERA_EFFECTS_SCENARIO_ID_V2"  -Option ReadOnly -Value 2834432
+
+Set-Variable -Name "VIDEO_RECORDING_DURATION"               -Option ReadOnly -Value 20
+Set-Variable -Name "NUMBER_OF_ITERATION"                    -Option ReadOnly -Value 10
+
+function VerifyVideoMode {
+    param ($assessmentScenario, $videoResName, $iter)
+
+    $timeStamp = Get-Date
+    $scenarioLogFolder = "CameraReliabilityTest\$assessmentScenario\iteration_$iter"
+    $wsev2PolicyState = CheckWSEV2Policy
+
+    # Start collecting traces
+    StartTrace $scenarioLogFolder $timeStamp
+
+    StartVideoRecording $VIDEO_RECORDING_DURATION
+
+    # Check if frame server is stopped
+    CheckServiceState 'Windows Camera Frame Server'
+
+    # Stop the trace
+    StopTrace $scenarioLogFolder $timeStamp
+
+    $scenarioID = if ($wsev2PolicyState) {
+        $WSE_ALL_CAMERA_EFFECTS_SCENARIO_ID_V2
+    } else {
+        $WSE_ALL_CAMERA_EFFECTS_SCENARIO_ID_V1
+    }
+
+    return Verifylogs $scenarioLogFolder $scenarioID $timeStamp
+}
+
+function VerifyPhotoMode {
+    param ($iter)
+
+    $timeStamp = Get-Date
+    $scenarioLogFolder = "CameraReliabilityTest\photo\iteration_$iter"
+
+    # Start collecting traces
+    StartTrace $scenarioLogFolder $timeStamp
+
+    StartPhotoCapturing
+
+    # Check if frame server is stopped
+    CheckServiceState 'Windows Camera Frame Server'
+
+    # Stop the trace
+    StopTrace $scenarioLogFolder $timeStamp
+
+    $scenarioID = 0
+    return Verifylogs $scenarioLogFolder $scenarioID $timeStamp
+}
+
+function CameraReliabilityTest {
+    param ($iteration)
+
+    $wsev2PolicyState = CheckWSEV2Policy
+
+    # Tweak camera to use the highest resolution
+    $videoResName = SetHighestVideoResolutionInCameraApp
+
+    $assessmentScenario = if ($wsev2PolicyState) {
+        $WSE_ALL_CAMERA_EFFECTS_SCENARIO_V2
+    } else {
+        $WSE_ALL_CAMERA_EFFECTS_SCENARIO_V1
+    }
+
+    # Toggle all effects on
+    Write-Log -Message "Entering ToggleAIEffectsInSettingsApp function to toggle all effects On" -IsOutput
+    ToggleAIEffectsInSettingsApp -AFVal "On" `
+                                 -PLVal "On" `
+                                 -BBVal "On" -BSVal "False" -BPVal "True" `
+                                 -ECVal "On" -ECSVal "False" -ECEVal "True" `
+                                 -VFVal "Off" `
+                                 -CF "On" -CFI "False" -CFA "True" -CFW "False"
+
+    # Check if frame server is stopped
+    CheckServiceState 'Windows Camera Frame Server'
+
+    for ($iter = 1; $iter -le $iteration; $iter++) {
+        Write-Log -Message "`nCameraReliabilityTest [$iter / $iteration] rounds" -IsHost
+
+        if (-not (VerifyVideoMode $assessmentScenario $videoResName $iter)) {
+            Write-Log -Message "Video mode for run ${iter}: FAIL" -IsHost -ForegroundColor Red
+        } else {
+            Write-Log -Message "Video mode for run ${iter}: PASS" -IsHost -ForegroundColor Green
+        }
+
+        if (-not (VerifyPhotoMode $iter)) {
+            Write-Log -Message "Photo mode for run ${iter}: FAIL" -IsHost -ForegroundColor Red
+        } else {
+            Write-Log -Message "Photo mode for run ${iter}: PASS" -IsHost -ForegroundColor Green
+        }
+    }
+
+    # Restore the default state for AI effects
+    Write-Log -Message "Entering ToggleAIEffectsInSettingsApp function to Restore the default state for AI effects" `
+              -IsOutput
+    ToggleAIEffectsInSettingsApp -AFVal "Off" `
+                                 -PLVal "Off" `
+                                 -BBVal "Off" -BSVal "False" -BPVal "False" `
+                                 -ECVal "Off" -ECSVal "False" -ECEVal "False" `
+                                 -VFVal "Off" `
+                                 -CF "Off" -CFI "False" -CFA "False" -CFW "False"
+}
+
+# Load helper library and initialize test
+.".\CheckInTest\Helper-library.ps1"
+InitializeTest 'CameraReliabilityTest' `
+               $targetMepCameraVer `
+               $targetMepAudioVer `
+               $targetPerceptionCoreVer
+
+# If the user does not specify the iteration parameter, default to NUMBER_OF_ITERATION.
+if ($iteration -eq 0) {
+   $iteration = $NUMBER_OF_ITERATION
+}
+
+Set-SystemSettingsInCamera
+CameraReliabilityTest $iteration
