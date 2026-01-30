16bda5d592ee1573d3704867b94e2169950ac941
WSEAutomationTests <jdugar@microsoft.com>
2026-01-30 11:21:37 -0800

    Minor changes

--- E2E/Helper/InitializeTest.ps1
+++ E2E/Helper/InitializeTest.ps1
@@ -18,7 +18,7 @@ function InitializeTest($TstsetNme, $targetMepCameraVer, $targetMepAudioVer, $ta
     New-Item -ItemType Directory -Force -Path $pathLogsFolder  | Out-Null
 
     # Delete all videos from Camera Roll before test starts
-    Clear-CameraRollVideos
+    Clear-CameraRollVideos 
 
     $Global:SequenceNumber = 0
     $Global:Results = '' | SELECT ScenarioName,fps,TotalNumberOfFrames,FramesAbove33ms,'AvgProcessingTimePerFrame(In ms)','MaxProcessingTimePerFrame(In ms)','MinProcessingTimePerFrame(In ms)','timetofirstframe(In secs)','CameraAppInItTime(In secs)','VoiceRecorderInItTime(In secs)','timetofirstframeForAudio(In secs)',FramesAbove33msForAudioBlur,'PeakWorkingSetSize(In MB)','AvgWorkingSetSize(In MB)','AvgNPUUsage(In %)','AvgCPUUsage(In %)' ,'AvgMemoryUsage(In GB)','BeforeNPUUsage(In %)','BeforeCPUUsage(In %)','BeforeMemoryUsage(In GB)',Status,ReasonForNotPass
